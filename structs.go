80e1420cc7ee65a29f9ee9decd9f4669c64b59e5
jonas747 <jonasr747@gmail.com>
2019-08-31 10:05:04 +0200

    added new channel types

--- structs.go
+++ structs.go
@@ -158,6 +158,8 @@ const (
 	ChannelTypeGuildVoice
 	ChannelTypeGroupDM
 	ChannelTypeGuildCategory
+	ChannelTypeGuildNews
+	ChannelTypeGuildStore
 )
 
 // A Channel holds all data related to an individual Discord channel.
